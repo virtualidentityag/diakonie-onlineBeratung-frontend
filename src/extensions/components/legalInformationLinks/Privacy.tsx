05c12d9df3620499951ec4a8ea273b9a33b07dd7
janrembold <janrembold@gmx.net>
2024-04-15 15:31:06 +0200

    feat: enable privacy pages from connecta

--- src/extensions/components/legalInformationLinks/Privacy.tsx
+++ src/extensions/components/legalInformationLinks/Privacy.tsx
@@ -4,14 +4,15 @@ import { useTenant } from '../../../..';
 import { LegalPageWrapper } from '../legalPageWrapper/LegalPageWrapper';
 import useDocumentTitle from '../../resources/utils/useDocumentTitle';
 
-export const Imprint = () => {
+export const Privacy = () => {
 	const [t] = useTranslation();
 	const tenant = useTenant();
-	useDocumentTitle(t('profile.footer.imprint'));
-
+	useDocumentTitle(t('profile.footer.dataprotection'));
 	return (
 		<LegalPageWrapper
-			content={tenant?.content?.impressum || t('profile.footer.imprint')}
+			content={
+				tenant?.content?.privacy || t('profile.footer.dataprotection')
+			}
 			className={'terms'}
 		/>
 	);
--- src/extensions/initApp.tsx
+++ src/extensions/initApp.tsx
@@ -3,15 +3,16 @@ import * as ReactDOM from 'react-dom';
 import { ThemeProvider } from '@mui/material';
 import { App } from '../components/app/app';
 import { Stage } from './components/stage/stage';
-import { Imprint } from './components/legalInformationLinks/Imprint';
-import { DataProtection } from './components/legalInformationLinks/DataProtection';
-import { config, legalLinks } from './resources/scripts/config';
+import { config, routePathNames } from './resources/scripts/config';
 import { UrlParamsProvider } from '../globalState/provider/UrlParamsProvider';
 import { RegistrationProvider } from '../globalState';
 import { lazy } from 'react';
 import '../resources/styles/mui-variables-mapping.scss';
 import theme from './theme';
 import { Redirect } from 'react-router-dom';
+import { TermsAndConditions } from './components/legalInformationLinks/TermsAndConditions';
+import { Privacy } from './components/legalInformationLinks/Privacy';
+import { Imprint } from './components/legalInformationLinks/Imprint';
 
 const Registration = lazy(() =>
 	import('./components/registration/Registration').then((m) => ({
@@ -53,17 +54,11 @@ ReactDOM.render(
 					)
 				},
 				{
-					route: {
-						path: legalLinks.imprint
-					},
-					component: Imprint
+					route: { path: routePathNames.termsAndConditions },
+					component: TermsAndConditions
 				},
-				{
-					route: {
-						path: legalLinks.privacy
-					},
-					component: DataProtection
-				}
+				{ route: { path: routePathNames.imprint }, component: Imprint },
+				{ route: { path: routePathNames.privacy }, component: Privacy }
 			]}
 			stageComponent={Stage}
 		/>
