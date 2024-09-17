83e2c3bdf0c010a8781c73cf78956a0f014c6187
Deborah Köpfer <koepferd@users.noreply.github.com>
2024-09-17 15:10:41 +0200

    feat(caritas-theme): remove deprecated registration

--- src/components/registration/prefillPostcode.ts
+++
@@ -1,6 +0,0 @@
-export const DEFAULT_POSTCODE = '00000';
-
-export const redirectToRegistrationWithoutAid = () => {
-	const url = window.location.href;
-	window.location.href = url.split('?')[0];
-};
