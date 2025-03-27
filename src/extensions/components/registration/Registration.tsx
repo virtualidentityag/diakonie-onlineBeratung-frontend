fef024d1e9af0dd87309104222d092de4fb75883
Igor Varga <igor.varga.external@virtual-identity.com>
2025-03-27 11:27:23 +0100

    fix: add password encoding for wizard registration

--- src/extensions/components/registration/Registration.tsx
+++ src/extensions/components/registration/Registration.tsx
@@ -190,6 +190,7 @@ export const Registration = () => {
 		const data = {
 			...registrationData,
 			...stepData,
+			password: encodeURIComponent(registrationData.password),
 			mainTopicId: registrationData.mainTopic.id.toString(),
 			topicId: registrationData.topic?.id?.toString(),
 			agencyId: registrationData.agency.id.toString(),
