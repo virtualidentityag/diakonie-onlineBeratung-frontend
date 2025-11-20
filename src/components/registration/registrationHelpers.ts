1b45e9c9251e602cd8e1219ba20ee32aeefa2793
Roman Archipov <roman.archipov@virtual-identity.com>
2025-11-20 15:25:50 +0100

    feat: diakonie-576 disallow spaces in username on registration
    * disallow spaces in username
    * show validity based on existing and new rule
    * adapt translations to hint the rule
    * remove obsolete code
    * mark deprecated component

--- src/components/registration/registrationHelpers.ts
+++ src/components/registration/registrationHelpers.ts
@@ -7,12 +7,14 @@ export type AccordionItemValidity =
 	| typeof VALIDITY_VALID
 	| typeof VALIDITY_INVALID;
 
-export const MIN_USERNAME_LENGTH = 5;
-
 export const isStringValidEmail = (email: string) =>
 	/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
 		email
 	);
+
+// minimum 5 characters, no spaces
+export const isStringValidUsername = (username: string) =>
+	/^(?!.*\s).{5,}$/.test(username);
 
 export const getOptionOfSelectedValue = (inputOptions, selectedValue) => {
 	return inputOptions.filter((item) => item.value === selectedValue)[0];
